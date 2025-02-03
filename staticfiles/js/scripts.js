0eac43c805852026b24863fc061aaa59e95fde21
Michael Pymer <mikepymer@hotmail.co.uk>
2025-02-03 11:34:21 +0000

    added limitied darkmode

--- staticfiles/js/scripts.js
+++ staticfiles/js/scripts.js
@@ -26,4 +26,24 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         });
     }
+
+    const darkModeToggle = document.getElementById('darkModeToggle');
+    const body = document.body;
+
+    // Check if dark mode is enabled in local storage
+    if (localStorage.getItem('darkMode') === 'enabled') {
+        body.classList.add('dark-mode');
+        darkModeToggle.textContent = 'Light Mode';
+    }
+
+    darkModeToggle.addEventListener('click', function () {
+        body.classList.toggle('dark-mode');
+        if (body.classList.contains('dark-mode')) {
+            localStorage.setItem('darkMode', 'enabled');
+            darkModeToggle.textContent = 'Light Mode';
+        } else {
+            localStorage.setItem('darkMode', 'disabled');
+            darkModeToggle.textContent = 'Dark Mode';
+        }
+    });
 });
